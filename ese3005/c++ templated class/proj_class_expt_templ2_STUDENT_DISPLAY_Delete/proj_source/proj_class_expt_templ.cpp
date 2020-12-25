60fafe114b6e866e8fd48d5adf4505d45488838b
vishalhasrajani <vish9221@gmail.com>
2020-12-25 17:53:47 -0500

    Display and delete the record from the database

---
+++ ese3005/c++ templated class/proj_class_expt_templ2_STUDENT_DISPLAY_Delete/proj_source/proj_class_expt_templ.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : proj_class_expt_templ.cpp
+// Author      : takis
+// Version     :
+// Copyright   : copyright (C) 2020 emad studio inc.
+// Description : code to experiment with templated classes
+//============================================================================
+
+#include <iostream>
+#include "proj_classes.h"	// main templated class
+#include "students.h"		// Student class
+//#include "toys.h"			// Toys class
+#include <string>
+
+using namespace std;
+// only use "using namespace" in your main code file
+
+
+
+int main()
+{
+	// declare a student-database object, load from standard input
+	DB<Student> myStudentDB;
+
+	// experiment with adding and deleting object from database
+
+	// your code goes here
+
+	// print out the data
+	myStudentDB.display();
+	myStudentDB.del();
+	cout<< "---------------After deletion---------------"<<endl;
+	myStudentDB.display();
+
+	// exit
+	cout << endl << myStudentDB.numelements() << " processed." << endl;
+
+
+	return 0;
+}
