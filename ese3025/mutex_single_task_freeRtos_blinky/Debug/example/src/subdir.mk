72b36ebed03269f312de297f74ebf770ddac687a
vishalhasrajani <vish9221@gmail.com>
2020-12-26 12:08:54 -0500

    Mutex single task FreeRtos Blinky

---
+++ ese3025/mutex_single_task_freeRtos_blinky/Debug/example/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../example/src/cr_startup_lpc175x_6x.c \
+../example/src/mutex_single_task_freertos_blinky_RGB.c \
+../example/src/sysinit.c 
+
+OBJS += \
+./example/src/cr_startup_lpc175x_6x.o \
+./example/src/mutex_single_task_freertos_blinky_RGB.o \
+./example/src/sysinit.o 
+
+C_DEPS += \
+./example/src/cr_startup_lpc175x_6x.d \
+./example/src/mutex_single_task_freertos_blinky_RGB.d \
+./example/src/sysinit.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+example/src/%.o: ../example/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -I"/home/star/Documents/MCUXpresso_11.2.0_4120/workspace/lpc_chip_175x_6x/inc" -I"/home/star/Documents/MCUXpresso_11.2.0_4120/workspace/lpc_board_nxp_lpcxpresso_1769/inc" -I"/home/star/Documents/MCUXpresso_11.2.0_4120/workspace/mutex_single_task_freeRtos_blinky/example/inc" -I"/home/star/Documents/MCUXpresso_11.2.0_4120/workspace/mutex_single_task_freeRtos_blinky/freertos/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m3 -mthumb -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ ese3025/mutex_single_task_freeRtos_blinky/example/src/mutex_single_task_freertos_blinky_RGB.c
@@ -0,0 +1,128 @@
+/*
+ * @brief FreeRTOS Blinky example
+ *
+ * @note
+ * Copyright(C) NXP Semiconductors, 2014
+ * All rights reserved.
+ *
+ * @par
+ * Software that is described herein is for illustrative purposes only
+ * which provides customers with programming information regarding the
+ * LPC products.  This software is supplied "AS IS" without any warranties of
+ * any kind, and NXP Semiconductors and its licensor disclaim any and
+ * all warranties, express or implied, including all implied warranties of
+ * merchantability, fitness for a particular purpose and non-infringement of
+ * intellectual property rights.  NXP Semiconductors assumes no responsibility
+ * or liability for the use of the software, conveys no license or rights under any
+ * patent, copyright, mask work right, or any other intellectual property rights in
+ * or to any products. NXP Semiconductors reserves the right to make changes
+ * in the software without notification. NXP Semiconductors also makes no
+ * representation or warranty that such application will be suitable for the
+ * specified use without further testing or modification.
+ *
+ * @par
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation is hereby granted, under NXP Semiconductors' and its
+ * licensor's relevant copyrights in the software, without fee, provided that it
+ * is used in conjunction with NXP Semiconductors microcontrollers.  This
+ * copyright, permission, and disclaimer notice must appear in all copies of
+ * this code.
+ */
+
+#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdint.h>
+#include "semphr.h"
+
+// GLOBAL PARAMETER
+uint8_t red_led = red;
+uint8_t green_led = green;
+uint8_t blue_led = blue;
+
+xSemaphoreHandle Mutex_h;  // Mutex Handler
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* Sets up system hardware */
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Initial red LED,green LED and blue LED states are off */
+	Board_LED_Set(red, false);
+	Board_LED_Set(green, false);
+	Board_LED_Set(blue, false);
+}
+
+/* LED1 toggle thread */
+static void vLEDTask(void *pvParameters) {
+
+	uint8_t LED = *((uint8_t*) pvParameters); // LED Color Parameter
+		if (LED == green)
+		{
+			// Block for 2 sec
+			vTaskDelay(2*configTICK_RATE_HZ );
+		}
+
+		if (LED == blue)
+		{
+			// Block for 4 sec
+			vTaskDelay(4*configTICK_RATE_HZ);
+		}
+
+	while (1) {
+		xSemaphoreTake(Mutex_h,1000);	// Take the Mutex
+
+		Board_LED_Set(LED, true); // led ON
+		vTaskDelay(configTICK_RATE_HZ );//1sec delay
+		Board_LED_Set(LED, false);// led OFF
+
+		xSemaphoreGive(Mutex_h);		   // Release the Mutex
+
+		vTaskDelay(5*configTICK_RATE_HZ );//5sec delay
+
+
+	}
+}
+
+int main(void)
+{
+	prvSetupHardware();
+
+	Mutex_h = xSemaphoreCreateMutex();
+
+	/* LED1 toggle thread */
+	xTaskCreate(vLEDTask, (signed char *) "vTaskLed1",
+				configMINIMAL_STACK_SIZE, (void *)&red_led, (tskIDLE_PRIORITY + 3UL),
+				(xTaskHandle *) NULL);
+
+	/* LED2 toggle thread */
+	xTaskCreate(vLEDTask, (signed char *) "vTaskLed2",
+				configMINIMAL_STACK_SIZE, (void *)&green_led, (tskIDLE_PRIORITY + 2UL),
+				(xTaskHandle *) NULL);
+	/* LED3 toggle thread */
+	xTaskCreate(vLEDTask, (signed char *) "vTaskLed3",
+					configMINIMAL_STACK_SIZE, (void *)&blue_led, (tskIDLE_PRIORITY + 1UL),
+					(xTaskHandle *) NULL);
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
+
+/**
+ * @}
+ */
